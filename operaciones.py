6c3b5111450d691c16739b60a1af12801f670857
Cesar-i-Nada <cesar.alvarado.vega@gmail.com>
2025-04-16 14:40:50 -0600

    actualizacionList1test

--- operaciones.py
+++ operaciones.py
@@ -1,28 +1,14 @@
-def suma(a, b):
-    return a + b
-resultado = suma()
+def sumar(numero, numero2):
+    return (numero + numero2)
+   
+def resta(numero, numero2):
+    return (numero - numero2)
 
-print(total)    
+def multiplicar(numero, numero2):
+    return (numero * numero2)
 
+def dividir(numero, numero2):
+    return (numero / numero2)
 
-def resta(a, b):
-    return a - b
-resultado = resta()
-
-print(total)
-    
-
-def multiplicar(a, b):
-    return a * b
-resultado = multiplicar()
-
-print(total)
-
-
-def dividir(a, b):
-    return a / b
-resultado = dividir()
-
-print(total)
 
 
--- test_operaciones.py
+++ test_operaciones.py
@@ -1,1 +1,13 @@
-from operaciones import suma, resta, multiplicar, dividir 
+from operaciones import sumar, resta, multiplicar, dividir
+
+def test_sumar():
+    assert sumar(8, 2) ==10
+    
+def test_resta():
+    assert resta(12, 2) ==10
+    
+def test_multiplicar():
+    assert multiplicar(5, 2) ==10
+    
+def test_dividir():
+    assert dividir(numero2=0,numero=20) == 0
